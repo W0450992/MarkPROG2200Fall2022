f5e2706368575a70c78fa6e10412ddea28446f6f
Mark Jamieson-Simmons <W0450992@nscc.ca>
2022-09-29 20:58:23 -0300

    passcode complete

---
+++ Lab1/Module1/Section2/Passcode/Passcode/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Passcode
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter the passcode");
+            var code = Console.ReadLine();
+
+            if (code == "secret")
+            {
+                Console.WriteLine("Authenticated");
+                Console.WriteLine("Change passcode? (yes or no)");
+                var yesOrNo = Console.ReadLine();
+                if (yesOrNo == "yes")
+                {
+                    Console.WriteLine("Type new passcode.");
+                    code = Console.ReadLine();
+                }
+
+
+            }
+            else
+            {
+                Console.WriteLine("Wrong passcode.");
+            }
+
+        }
+    }
+}
