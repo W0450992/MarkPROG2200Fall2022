9e7a222bc17d3fdcd4864ccf61331d61ad3bd8b9
Mark Jamieson-Simmons <W0450992@nscc.ca>
2022-09-29 19:25:45 -0300

    added lab1 folder

---
+++ Projects/FirstApp/FirstApp/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hi Mark");
+            // Class_Name objectName = new Class_Name();
+            Department department = new Department();
+
+
+        }
+    } // end of program class
+    public class NewStudent
+    {
+        public int Id { get; set; }
+    }
+        
+    
+}
---
+++ Projects/HelloWorld1/HelloWorld1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Humanizer;
+
+namespace HelloWorld1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(TimeSpan.FromMilliseconds(1299630020).Humanize(3));
+            Console.WriteLine(TimeSpan.FromMilliseconds(3603001).Humanize(3));
+            Console.WriteLine(TimeSpan.FromMilliseconds(8157019263).Humanize(3));
+
+
+        }
+    }
+}
